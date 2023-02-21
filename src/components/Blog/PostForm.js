4ade6c996f801260596cc3376de099776df350e9
cpaschall <cpaschall216@gmail.com>
2023-02-21 18:51:30 -0500

    Creating PostForm and AddPost components

---
+++ src/components/Blog/PostForm.js
@@ -0,0 +1,11 @@
+import './PostForm.css';
+
+const PostForm = () => {
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default PostForm;
---
+++ src/components/Pages/CreatePost.js
@@ -0,0 +1,11 @@
+import './CreatePost.css';
+
+const CreatePost = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CreatePost;
