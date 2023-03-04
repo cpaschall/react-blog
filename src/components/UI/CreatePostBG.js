6ade4afae640a03993b3b7d8fc2b4a46a05d7514
cpaschall <cpaschall216@gmail.com>
2023-03-03 19:51:35 -0500

    adding style to background page

--- src/components/UI/CreatePostBG.js
+++ src/components/UI/CreatePostBG.js
@@ -4,26 +4,26 @@ const CreatePostBG = (props) => {
     return (
         <div className='cpbg-main'>
             <div className='cpbg-text'>
-                <div className='cpbg-text__16FF00'>kdfvkljfdhdf</div>
-                <div className='cpbg-text__FB2576'>ldkfd</div>
-                <div className='cpbg-text__FFFBAC'>akdj falkd akjdfka ljsdf</div>
-                <div className='cpgb-text__FF4949'>lajdflkajdf jkadfkjlafhakjldf kajdsfkaljdfhalkfdh</div>
-                <div className='cpbg-text__F2E7D5'>kjadfkjladhfalkj aksjdfnkajldfalk laksdjfalkjdfalkdf lkajdsfnlkjadflkasjbdf</div>
-                <div className='cpbg-text__8D9EFF'>                 alkdjsfalksdjf lkadsjfakjlsdfhakldjf jaldsfhlkajsdflakd</div>
-                <div className='cpbg-text__FFED00'>alkjsdfklajsdf    </div>
-                <div className=''>lkasjdfklajdfakjld aklsjdfklajsfdlkasjdfh lkajsdfkajldfhkaljsdfh lkjadslkajsdhfkljdhfsa </div>
-                <div className='cpbg-text__C47AFF'>alksjdfklajdsfklajsdhf alkjdsfkaljdfhalksjdh </div>
-                <div className='cpbg-text__D2001A'>         klajdsfkjsadhfjksald alsdjfhklsjdf </div>
-                <div className='cpbg-text__'>tyertyetyetrerty qazxccvbhjyjyk  uyityutyjfghghnjgf  phjngdsfsdfsdfdf </div>
-                <div className='cpbg-text__'></div>
-                <div className='cpbg-text__'>czxcvzxcvzxcvzxcv fdfghtrnghjfhf </div>
-                <div className='cpbg-text__'>uyuty dgdfghfdghdfgh </div>
-                <div className='cpbg-text__'>pkpjljjkmhjnhgg  uitfgbfbgf qweddssads </div>
-                <div className='cpbg-text__'>ydtvfvdrgdvfvd vdfvdvfvdfv </div>
-                <div className='cpbg-text__'>vcxcvx dhkfaksdfakdfh ioeutpyoertye qweqweq </div>
-                <div className='cpbg-text__'>ptpupt bvxmv uweyrtweuywt </div>
-                <div className='cpbg-text__'>mnbxcmvbkuyrgwe wrqdtyqdrcsg </div>
-                <div className='cpbg-text__'>werwuoiwruwtherhn </div><div className='cpbg-text__'>oiteurponkvnb qwqw cvxcvbxvbvc reter xf rtr yuytyh </div>
+                <div className='cpbg-text__16FF00'>kdfvk</div>
+                <div className='cpbg-text__FB2576'>ldk</div>
+                <div className='cpbg-text__FFFBAC'>akdjfdfa</div>
+                <div className='cpgb-text__FF4949'>jdf</div>
+                <div className='cpbg-text__F2E7D5'>kjadfkjl</div>
+                <div className='cpbg-text__8D9EFF'>fj</div>
+                <div className='cpbg-text__FFED00'>alajs</div>
+                <div className='cpbg-text__DF2E38'>lkasjd</div>
+                <div className='cpbg-text__C47AFF'>alks</div>
+                <div className='cpbg-text__D2001A'>klajdsfk </div>
+                <div className='cpbg-text__'>fu</div>
+                <div className='cpbg-text__'>dsdsds</div>
+                <div className='cpbg-text__'>xcvzxcv</div>
+                <div className='cpbg-text__'>uyuty</div>
+                <div className='cpbg-text__'>pkpjljjk</div>
+                <div className='cpbg-text__'>vdfvdvfvdfv</div>
+                <div className='cpbg-text__'>dfakdfh</div>
+                <div className='cpbg-text__'>bvxmv</div>
+                <div className='cpbg-text__'>mnbxcmvbk</div>
+                <div className='cpbg-text__'>wer</div><div className='cpbg-text__'>oiteuryh</div>
             </div>
             {props.children}
         </div>
